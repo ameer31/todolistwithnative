56b4b987029fdbbe9652f0e2edfdffdadace1d08
ameer31 <ameer.hamza7433@gmail.com>
2023-10-24 03:33:01 +0500

    almost done

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
+import styles from './style'
 
 export default function App() {
+  const [list, setList] = useState([])
+  const [UserInput, setUserInput] = useState('')
+  const [editMode, setEditMode] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState(null)
+
+  const addItem = () => {
+    const tempList = [...list]
+    tempList.push(UserInput)
+    setList(tempList)
+    setUserInput('');
+
+  }
+  const deleteItem = () => {
+    const tempList = [...list];
+    tempList.splice(index, 1);
+    setList(tempList);
+  }
+
+
+  const editItem = () => {
+    const value = list[index];
+    setUserInput(value);
+    setEditMode(true);
+    setCurrentIndex(index);
+  }
+
+  const allItem = () => {
+    setList([])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Button
+        onPress={allItem}
+        title="DeleteAllItem"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setUserInput(text)}
+        placeholder='Type any item'
+        value={UserInput}
+      />
+
+      <Button
+        onPress={addItem}
+        title="Add Item"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+
+      />
+
+      {list.map(item  => {
+        return <Text> {item}<Button
+          onPress={() => editItem(index)}
+          title="Edit Item"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        /> <Button
+            onPress={ () => deleteItem(index)}
+            title="Delet Item"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          /></Text>
+
+      })}
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style.js
@@ -0,0 +1,25 @@
+import { Button, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',n
+    // justifyContent: 'center',
+  },
+  input: {
+    alignItems:'center',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 20,
+  },
+ Button:{
+  
+    backgroundColor:'#fff',
+    border:1
+    
+  }
+});
+
+export default styles
